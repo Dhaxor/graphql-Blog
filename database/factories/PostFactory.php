8c4bf29b5e96a6da26416eec4f7fe105822c7f66
Gain John <emperorduke5@gmail.com>
2020-11-17 22:04:34 +0100

    Initial commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+
+use App\Post;
+use App\User;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'author_id' => factory(User::class)->create()->id,
+        'title' => $faker->sentence,
+        'content' => $faker->text
+    ];
+});
